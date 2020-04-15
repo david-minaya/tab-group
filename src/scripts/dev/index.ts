da19fa11d1961fecb52bfa81d78dbfaa3f3f8bb9
david-minaya <davidminaya04@gmail.com>
2020-04-14 20:36:34 -0400

    Add the dev page
    This page is used to render the components while they are developing.

---
+++ src/scripts/dev/index.ts
@@ -0,0 +1,3 @@
+import { insertDev } from './dev';
+
+insertDev();
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
     'tab-bar': './src/content-scripts/tab-bar',
     'add-modal': './src/content-scripts/add-modal',
     'popup': './src/scripts/popup.tsx',
-    'tab-bar-page': './src/scripts/tab-bar-page.tsx'
+    'tab-bar-page': './src/scripts/tab-bar-page.tsx',
+    'dev': './src/scripts/dev'
   },
   output: {
     filename: '[name].js'
@@ -31,8 +32,13 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       filename: 'tab-bar-page.html',
-      chunks: ['tab-bar-page'],
-      title: 'Tab bar'
+      title: 'Tab bar',
+      chunks: ['tab-bar-page']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'dev.html',
+      title: 'dev',
+      chunks: ['dev']      
     })
   ],
   module: {
