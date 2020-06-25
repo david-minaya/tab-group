e1b65fcabaa84dfa59188bf3fc9ca70ecd37d796
david-minaya <davidminaya04@gmail.com>
2020-06-24 20:39:14 -0400

    Move the code of the main-pane and navigation-pane to the tab-bar-page

--- src/components/tab-bar-page/tab-bar-page.tsx
+++ src/components/tab-bar-page/tab-bar-page.tsx
@@ -1,15 +1,41 @@
 import * as React from 'react';
 import * as style from './tab-bar-page.css';
-import { MainPane } from '../main-pane';
-import { NavigationPane } from '../navigation-pane';
 import { initializeIcons } from '@uifabric/icons';
+import * as Storage from '../../storage';
+import { TabGroup } from '../tab-group';
+
 initializeIcons();
 
 export function TabBarPage() {
+
+  const storage = React.useMemo(() => new Storage.Storage(new Storage.LocalStorage()), []); 
+  const [tabsGroups, setTabsGroups] = React.useState([]);
+
+  React.useEffect(() => {
+    getTabsGroups();
+  });
+
+  async function getTabsGroups() {
+    const tabsGroups = await storage.getTabsGroup();
+    const filteredTabsGroupd = tabsGroups.filter(tabGroup => !tabGroup.isTemp);
+    const tabsGroupsRevesed = filteredTabsGroupd.reverse();
+    setTabsGroups(tabsGroupsRevesed);
+  }
+
   return (
     <div className={style.index}>
-      <NavigationPane/>
-      <MainPane/>
+      <div className={style.navigationPane}>
+        <div className={style.title}>Tab Group</div>
+      </div>
+      <div className={style.mainPane}>
+        <div className={style.list}>
+          {
+            tabsGroups.map(tabGroup => {
+              return <TabGroup key={tabGroup.id} tabGroup={tabGroup} />;
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
